7d22476c04aa9024933af88b4cea17560eee63dd
Jonathan Mardones <jmardones.94@gmail.com>
2022-11-10 20:36:28 -0300

    agregar formulario para crear y persistencia con ls

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
-const items = [
-  {
-    nombre: "Zapatillas",
-    descripcion: "Estas zapatillas son de trekking y son impermeables",
-    precio: "100000",
-    imagen:
-      "https://images.unsplash.com/photo-1582898967731-b5834427fd66?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-  },
-  {
-    nombre: "Poleron",
-    descripcion: "Este poleron blanco térmico manga larga es 100% algodon",
-    precio: "40000",
-    imagen:
-      "https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072&q=80",
-  },
-  {
-    nombre: "Pantalon",
-    descripcion: "Este pantalon es deportivo",
-    precio: "59990",
-    imagen:
-      "https://images.unsplash.com/photo-1552902865-b72c031ac5ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
-  },
-]
+// let items = [
+//   {
+//     nombre: "Zapatillas",
+//     descripcion: "Estas zapatillas son de trekking y son impermeables",
+//     precio: "100000",
+//     imagen:
+//       "https://images.unsplash.com/photo-1582898967731-b5834427fd66?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+//   },
+//   {
+//     nombre: "Poleron",
+//     descripcion: "Este poleron blanco térmico manga larga es 100% algodon",
+//     precio: "40000",
+//     imagen:
+//       "https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072&q=80",
+//   },
+//   {
+//     nombre: "Pantalon",
+//     descripcion: "Este pantalon es deportivo",
+//     precio: "59990",
+//     imagen:
+//       "https://images.unsplash.com/photo-1552902865-b72c031ac5ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+//   },
+// ]
+
+const items = JSON.parse(localStorage.getItem("items"))
 
 const listaArticulos = document.getElementById("lista-articulos")
 
-items.forEach((item) => {
-  agregarProducto(item)
-})
+pintarItems(items)
 
-function agregarProducto(producto) {
+function pintarItem(producto) {
   const card = document.createElement("div")
   card.classList.add("card", "m-3")
   card.style.width = "18rem"
@@ -49,3 +49,55 @@ function agregarProducto(producto) {
 
   listaArticulos.appendChild(card)
 }
+
+function pintarItems(productos) {
+  listaArticulos.innerHTML = ""
+  productos.forEach(pintarItem)
+}
+
+const btnSearch = document.getElementById("btn-search")
+const inputSearch = document.querySelector("#input-search")
+
+btnSearch.addEventListener("click", (e) => {
+  e.preventDefault()
+  const productos = items.filter((item) => {
+    return item.nombre.toLowerCase().includes(inputSearch.value.toLowerCase())
+  })
+
+  pintarItems(productos)
+})
+
+const btnForm = document.querySelector("#btn-form")
+
+btnForm.addEventListener("click", (e) => {
+  e.preventDefault()
+  const nombreInput = document.querySelector("#nombre")
+  const descripcionInput = document.querySelector("#descripcion")
+  const precioInput = document.querySelector("#precio")
+  const imagenInput = document.querySelector("#imagen")
+
+  if (
+    !nombreInput.value ||
+    !descripcionInput.value ||
+    !precioInput.value ||
+    !imagenInput.value
+  ) {
+    alert("Hay un campo vacío")
+    return
+  }
+
+  items.push({
+    nombre: nombreInput.value,
+    descripcion: descripcionInput.value,
+    precio: precioInput.value,
+    imagen: imagenInput.value,
+  })
+
+  nombreInput.value = ""
+  descripcionInput.value = ""
+  precioInput.value = ""
+  imagenInput.value = ""
+
+  pintarItems(items)
+  localStorage.setItem("items", JSON.stringify(items))
+})
